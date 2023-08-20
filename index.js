8ae49b5e56712be1bee98e5f512e41d58dbba8bf
Rajesh karwasara <140477625+Rajeshkarwasara@users.noreply.github.com>
2023-08-20 10:14:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let r="hello 'rajesh' how  Rajesh are you"
+console.log(r);
+let r2=r.replace(/Rajesh/gi,"raju")
+console.log(r2);
+
+
+let sum="string";
+let sum2="stringg";
+let b=sum+" sum2"+" rahu";
+console.log(b);
+let sums=sum.concat(" " ,sum2);
+console.log(sums);
